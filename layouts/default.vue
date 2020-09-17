ccd74d281362796798e40e6352522fa901b2c7c3
Tony Batty <tony.batty@so.energy>
2020-09-17 12:47:31 +0100

    add meta info

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,19 @@ export default {
   components: {
     NavBar,
   },
+  head() {
+    return {
+      titleTemplate: '%s - Real World Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Where you can find all the events taking place in your neighborhood',
+        },
+      ],
+    }
+  },
 }
 </script>
 
--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,20 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can crate a new event in your neighborhood',
+        },
+      ],
+    }
+  },
+}
+</script>
