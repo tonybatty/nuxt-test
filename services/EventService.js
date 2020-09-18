c3773105b76a2ce28dbe6e23cc6f4e282e4ab689
Tony Batty <tony.batty@so.energy>
2020-09-18 15:05:50 +0100

    update json server url

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://my-json-server.typicode.com/tonybatty/nuxt-test',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
